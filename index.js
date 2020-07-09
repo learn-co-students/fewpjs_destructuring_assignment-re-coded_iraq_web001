75cdf2d5719c586e42179f95e90e97bf3b3ce8fe
Dunia Nasr <dunia.nasr777@gmail.com>
2020-07-09 16:03:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const farmAnimals = 'cow horse sheep pig chicken'
+const farmAnimals = ["cow", "horse" ,"sheep", "pig", "chicken"]
+const sounds = ['moo', 'nai', 'ba', 'oink', 'cluck']
+const [mooo, nigh, baaa, oiink, cluck] 
+console.log(mooo, nigh, baaa, oiink, cluck)
+
+
+const [Bessie, , Dolly, Babe, Little] = farmAnimals
+console.log(Bessie, Dolly, Babe, Little)
+
+
+const [black_and_white, , black, pink, ,] = farmAnimals
+console.log(black_and_white, black, pink)
+
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+console.log(red, orange, yellow, green, blue, indigo, violet)
+
+
+const [r, o, y, g, b, , v] = colors
+console.log(r, o, y, g, b,v)
+
+const [, , , , , indg, ]= colors
+console.log(indg)
 
 const muppet = {
   name: 'Kermit',
@@ -9,6 +31,13 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+
+const { name, color, song, job, partner} = muppet;
+console.log(name);
+console.log(color);
+console.log(song);
+console.log(job);
+console.log(partner);
 
 const k_muppet = {
   k_name: 'Kermit',
@@ -24,6 +53,18 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
+
+
+const {
+        k_album: {
+          the_muppet_movie: { song_2, song_4
+          }
+    }, k_color, k_job, k_partner} = k_muppet;
+console.log(k_color);
+console.log(song_2);
+console.log(song_4);
+console.log(k_job);
+console.log(k_partner);
 
 // Strings
 
